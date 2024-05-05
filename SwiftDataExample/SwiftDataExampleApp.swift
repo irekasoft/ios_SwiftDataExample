e55f04b14316b998445c234613bd3e35f6d7272d
Hijazi MBA M1 <hijazi@irekasoft.com>
2024-05-05 12:48:45 +0800

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Hijazi on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
