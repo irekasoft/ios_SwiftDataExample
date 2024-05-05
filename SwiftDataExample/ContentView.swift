67294e0ace60267f9f740b924ada6ce0d34d1034
Hijazi MBA M1 <hijazi@irekasoft.com>
2024-05-05 16:02:46 +0800

    add readme

--- SwiftDataExample/ContentView.swift
+++ SwiftDataExample/ContentView.swift
@@ -6,18 +6,199 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment (\.modelContext) var context
+    
+    @State private var isShowingItemSheet = false
+    
+//    @Query (filter: #Predicate<Expense> { $0.value > 1000 }, sort: \Expense.date)
+//    var expenses: [Expense]
+    
+    @Query (sort: \Expense.date, order: .reverse) var expenses: [Expense]
+    
+    @State private var expenseToEdit: Expense?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(expenses) { expense in
+                    ExpenseCell(expense: expense)
+                        .onTapGesture {
+                            expenseToEdit = expense
+                        }
+                }
+                .onDelete { indexSet in
+                 
+                    for index in indexSet {
+                        context.delete(expenses[index])
+                    }
+                    
+                }
+            }
+            .navigationTitle ("Expenses")
+            .navigationBarTitleDisplayMode(.inline)
+            .sheet(isPresented: $isShowingItemSheet) {
+                AddExpenseSheet()
+            }
+            .sheet(item: $expenseToEdit) { expense in
+                EditExpenseSheet(expense: expense)
+            }
+            .toolbar {
+                
+                Button ("Add Expense", systemImage: "plus") {
+                    isShowingItemSheet = true
+                }
+                
+            }
+            .overlay {
+                
+                if expenses.isEmpty {
+                
+                    ContentUnavailableView(label: {
+                        Label("No Expenses", systemImage: "dollarsign.arrow.circlepath")
+                    }, description: {
+                        Text("Start adding expenses to see your list.")
+                    }, actions: {
+                        Button ("Add Expense") { isShowingItemSheet = true }
+                    })
+                    .offset(y: -60)
+                    
+                }
+                
+            }
+            
         }
-        .padding()
+        
     }
+    
 }
+
+
+struct ExpenseCell: View {
+    
+    let expense: Expense
+    
+    var body: some View {
+        HStack {
+            
+            Text(expense.name)
+            
+            Spacer()
+            
+            Text(String(format: "%.2f", expense.value ?? 0.0))
+            
+        }
+    }
+    
+}
+
+
+struct AddExpenseSheet: View {
+    
+    var expense: Expense?
+    
+    @Environment (\.modelContext) var context
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var name: String = ""
+
+    @State private var date:Date = .now
+    
+    @State private var value: Double? = nil
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            Form {
+                
+                TextField( "Expense Name", text: $name)
+                
+                DatePicker("Date", selection: $date, displayedComponents: .date)
+                
+                TextField("Value", value: $value, format: .currency (code: ""))
+                    .keyboardType(.decimalPad)
+                    .navigationTitle("New Expense")
+                    .navigationBarTitleDisplayMode(.large)
+                    .toolbar {
+                        
+                        ToolbarItemGroup(placement: .topBarLeading) {
+                            Button ("Cancel") { dismiss () }
+                        }
+                        ToolbarItemGroup(placement:.topBarTrailing){
+                            Button ("Save") {
+                                
+                                if (value == nil) {
+                                    return
+                                }
+                                
+                                let expense = Expense(name: name, date: date, value: value!)
+                                
+                                context.insert(expense)
+                                
+//                                try! context.save()
+                                
+                                dismiss ()
+                                
+                            }
+                        }
+                        
+                    }
+                
+            }
+        }
+    }
+                
+}
+        
+struct EditExpenseSheet: View {
+    
+    @Bindable var expense: Expense
+    
+    @Environment (\.modelContext) var context
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var name: String = ""
+
+    @State private var date:Date = .now
+    
+    @State private var value: Double? = nil
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            Form {
+                
+                TextField( "Expense Name", text: $expense.name)
+                
+                DatePicker("Date", selection: $expense.date, displayedComponents: .date)
+                
+                TextField("Value", value: $expense.value, format: .currency (code: ""))
+                    .keyboardType(.decimalPad)
+                    .navigationTitle("New Expense")
+                    .navigationBarTitleDisplayMode(.large)
+                    .toolbar {
+                        
+                        ToolbarItemGroup(placement:.topBarTrailing){
+                            Button ("Done") {
+                                dismiss ()
+                            }
+                        }
+                        
+                    }
+                
+            }
+        }
+    }
+                
+}
+  
 
 #Preview {
     ContentView()
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
-#  <#Title#>
+#  SwiftDataExample
+
+data: https://www.youtube.com/watch?v=mvXFGikltPc
+
+wwdc23: https://developer.apple.com/videos/play/wwdc2023/10195/
+
+https://medium.com/@nayankhadase97/swiftdata-the-future-of-data-persistence-in-swift-7f23659ff0b6
 
